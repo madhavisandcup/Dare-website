b26b8ee15ee4039b50f41ee77a5e80bc529624ed
Madhavi <madhavisandcup@gmail.com>
2025-05-28 10:51:35 +0530

    header changes ongoingg

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <title>Dare - Mission and Vision</title>
 
     <!-- <link rel='icon' type='image/png' sizes='16x16' href='./images/favicon.svg'> -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="css/typography.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css" />
     <link href="css/responsive.css" rel="stylesheet" type="text/css" />
@@ -29,11 +30,77 @@
             </div>
             <div class="main-menu-wrapper">
                 <div class="collapse navbar-collapse container" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="">About</a>
-                        </li>
-                    </ul>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <h3 class="text_green">About</h3>
+                                    <a class="nav-link" href="">The Leader’s Story</a>
+                                    <a class="nav-link" href="">Mission & Vision</a>
+                                    <a class="nav-link" href="">APC Team</a>
+                                    <a class="nav-link" href="">Community Roots</a>
+                                </li>
+                            </ul>
+                             <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <h3 class="text_green">Vision & Mission</h3>
+                                    <a class="nav-link" href="">Pillars of Progress</a>
+                                    <a class="nav-link" href="">Track Record</a>
+                                    <a class="nav-link" href="">Policy Priorities</a>
+                                    <a class="nav-link" href="">Our Vision</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <h3 class="text_green">Be Part of the Change</h3>
+                                    <a class="nav-link" href="">Ongoing Initiatives</a>
+                                    <a class="nav-link" href="">Events & Rallies</a>
+                                    <a class="nav-link" href="">Volunteer with Us</a>
+                                    <a class="nav-link" href="">Join the Movement</a>
+                                </li>
+                            </ul>
+                             <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <h3 class="text_green">Updates</h3>
+                                    <a class="nav-link" href="">News & Announcements</a>
+                                    <a class="nav-link" href="">Monthly Highlights</a>
+                                    <a class="nav-link" href="">Impact Stories</a>
+                                    <a class="nav-link" href="">Social Media Highlights</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <h3 class="text_green">Join Us</h3>
+                                    <a class="nav-link" href="">Sign Up - For newsletter, volunteering, etc</a>
+                                    <a class="nav-link" href="">Donate - Support the mission financially</a>
+                                </li>
+                            </ul>
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <h3 class="text_green">Contact Us</h3>
+                                    <div class="header-social-icons">
+                                        <a href="">
+                                            <img src="./images/twitter.svg" alt="twitter" class="img-fluid social_v">
+                                        </a>
+                                         <a href="">
+                                            <img src="./images/linkedin.svg" alt="linkedin" class="img-fluid social_v">
+                                        </a>
+                                         <a href="">
+                                            <img src="./images/instagram.svg" alt="instagram" class="img-fluid social_v">
+                                        </a>
+                                         <a href="">
+                                            <img src="./images/facebook.svg" alt="facebook" class="img-fluid social_v">
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </nav>
