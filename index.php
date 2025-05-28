2aa606a09514920becb5e987c48c1f5ecc6c3f79
Madhavi <madhavisandcup@gmail.com>
2025-05-28 12:56:28 +0530

    latest-header changes

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
 <body>
-    
+
     <?php include 'header.php'; ?>
     <div class="homepage">
 
 
-    
-    
-    
-    <section class="vision-wrapper sec-space">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <h2>Familiar Facesdfdf, Rising Vision.</h2>
-                </div>
-            </div>
-        </div>
-    </section>
+    </div>
 
-</div>
-
-<?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
 
 </body>
