36aa3f9819c88c5b275b957f01882d667011bc06
Madhavi <madhavisandcup@gmail.com>
2025-05-28 15:56:25 +0530

    hero-banner changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <section class="container">
             <div class="hero-banner">
                 <div class="hero-title">
-                    <h1 class="text-white">
+                    <h1 class="text-white mb-0">
                         <span class="">
                             <span class="">Hope in</span>
                             <span class="pd_italic"><em>Motion</em><span class="text_green">,</span></span>
@@ -13,9 +13,22 @@
                         <span class="d-block">
                             <span class="pd_italic"><em>Leadership</em></span>
                             <span class="">in Action<span class="text_green">.</span>
-                        </span>
+                            </span>
                     </h1>
                     <p class="text-white">Oluwadare Owolabi leads with vision, compassion, and a promise to serve the people not power.</p>
+                </div>
+                <div class="connect-btn">
+                    <div class="main-btn-inner">
+                        <a href="#" class="main-btn position-relative"><span class="btn-text">Lets discuss an idea</span><span class="arrw-btn">
+                                <span class="arrws-inner d-block overflow-hidden"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="arrw-icon one" viewBox="0 0 16 16" fill="none">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M7.22918 0.853243C7.6559 0.426523 8.34775 0.426523 8.77447 0.853242L15.1484 7.22723C15.3534 7.43214 15.4685 7.71007 15.4685 7.99987C15.4685 8.28967 15.3534 8.56759 15.1484 8.77251L8.77447 15.1465C8.34775 15.5732 7.6559 15.5732 7.22918 15.1465C6.80246 14.7198 6.80246 14.0279 7.22918 13.6012L11.7378 9.09255H1.62784C1.02437 9.09255 0.535156 8.60334 0.535156 7.99987C0.535156 7.3964 1.02437 6.90719 1.62784 6.90719H11.7378L7.22918 2.39853C6.80246 1.97181 6.80246 1.27996 7.22918 0.853243Z" fill="#008751" />
+                                    </svg><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="arrw-icon two" viewBox="0 0 16 16" fill="none">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M7.22918 0.853243C7.6559 0.426523 8.34775 0.426523 8.77447 0.853242L15.1484 7.22723C15.3534 7.43214 15.4685 7.71007 15.4685 7.99987C15.4685 8.28967 15.3534 8.56759 15.1484 8.77251L8.77447 15.1465C8.34775 15.5732 7.6559 15.5732 7.22918 15.1465C6.80246 14.7198 6.80246 14.0279 7.22918 13.6012L11.7378 9.09255H1.62784C1.02437 9.09255 0.535156 8.60334 0.535156 7.99987C0.535156 7.3964 1.02437 6.90719 1.62784 6.90719H11.7378L7.22918 2.39853C6.80246 1.97181 6.80246 1.27996 7.22918 0.853243Z" fill="#008751" />
+                                    </svg></span></span></a>
+                    </div>
+                    <div class="apc">
+                        <img src="./images/apc-logo.svg" class="apc-logo" alt="apc-logo">
+                    </div>
                 </div>
             </div>
         </section>
