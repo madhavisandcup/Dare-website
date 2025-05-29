bcd7594714317d05a99978c5892a225234feb3ae
Madhavi <madhavisandcup@gmail.com>
2025-05-29 13:14:47 +0530

    ongoing event-section

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
     <script src="./js/bootstrap.min.js"></script>
     <script src="https://unpkg.com/gsap@3/dist/gsap.min.js"></script>
     <script src="https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="./js/circletype.min.js"></script>
     <script src="./js/custom.js"></script>
 </footer>
