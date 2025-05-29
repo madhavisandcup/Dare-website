cb5315fefc306dfcd517e4b2388946468a955e45
Madhavi <madhavisandcup@gmail.com>
2025-05-29 12:24:32 +0530

    ongoing on events-section

--- index.php
+++ index.php
@@ -42,9 +42,90 @@
 
             </div>
         </section>
-
     </div>
 
+    <section class="events-wrapper sec-space overflow-hidden">
+        <div class="container">
+            <div class="center-heading text-center">
+                <h2 class="vision-title w-100">Be Part of the <i class="pd_italic">Movement</i><span class="dor-anima ted text_green">.</span></h2>
+                <p class="vision-desc p-0">See where we’ll be next rallies, town halls, and moments that matter.</p>
+            </div>
+            <div class="event-tabs-wrapper">
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <button class="tabs-name nav-link active" id="nav-jan-tab" data-bs-toggle="tab" data-bs-target="#nav-jan" type="button" role="tab" aria-controls="nav-jan" aria-selected="true">
+                        <h5>Jan</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-fab-tab" data-bs-toggle="tab" data-bs-target="#nav-fab" type="button" role="tab" aria-controls="nav-fab" aria-selected="false">
+                        <h5>Feb</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-march-tab" data-bs-toggle="tab" data-bs-target="#nav-march" type="button" role="tab" aria-controls="nav-march" aria-selected="false">
+                        <h5>Mar</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-apr-tab" data-bs-toggle="tab" data-bs-target="#nav-apr" type="button" role="tab" aria-controls="nav-apr" aria-selected="false">
+                        <h5>Apr</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-may-tab" data-bs-toggle="tab" data-bs-target="#nav-may" type="button" role="tab" aria-controls="nav-may" aria-selected="false">
+                        <h5>May</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-jun-tab" data-bs-toggle="tab" data-bs-target="#nav-jun" type="button" role="tab" aria-controls="nav-jun" aria-selected="false">
+                        <h5>Jun</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-july-tab" data-bs-toggle="tab" data-bs-target="#nav-july" type="button" role="tab" aria-controls="nav-july" aria-selected="false">
+                        <h5>Jul</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-aug-tab" data-bs-toggle="tab" data-bs-target="#nav-aug" type="button" role="tab" aria-controls="nav-aug" aria-selected="false">
+                        <h5>Aug</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-sep-tab" data-bs-toggle="tab" data-bs-target="#nav-sep" type="button" role="tab" aria-controls="nav-sep" aria-selected="false">
+                        <h5>Sep</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-oct-tab" data-bs-toggle="tab" data-bs-target="#nav-oct" type="button" role="tab" aria-controls="nav-oct" aria-selected="false">
+                        <h5>Oct</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-nov-tab" data-bs-toggle="tab" data-bs-target="#nav-nov" type="button" role="tab" aria-controls="nav-nov" aria-selected="false">
+                        <h5>Nov</h5>
+                    </button>
+                    <button class="tabs-name nav-link" id="nav-dec-tab" data-bs-toggle="tab" data-bs-target="#nav-dec" type="button" role="tab" aria-controls="nav-dec" aria-selected="false">
+                        <h5>Dec</h5>
+                    </button>
+                </div>
+
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="nav-jan" role="tabpanel" aria-labelledby="nav-jan-tab" tabindex="0">
+                        <div class="event-card">
+                            <div class="event_card_img">
+                                <img src="./images/events-image/culture-center.webp" alt="culture-center" class="card_img_size">
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="tab-pane fade" id="nav-fab" role="tabpanel" aria-labelledby="nav-fab-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-march" role="tabpanel" aria-labelledby="nav-march-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-apr" role="tabpanel" aria-labelledby="nav-apr-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-may" role="tabpanel" aria-labelledby="nav-may-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-jun" role="tabpanel" aria-labelledby="nav-jun-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-july" role="tabpanel" aria-labelledby="nav-july-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-aug" role="tabpanel" aria-labelledby="nav-aug-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-sep" role="tabpanel" aria-labelledby="nav-sep-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-oct" role="tabpanel" aria-labelledby="nav-oct-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-nov" role="tabpanel" aria-labelledby="nav-nov-tab" tabindex="0">...</div>
+
+                    <div class="tab-pane fade" id="nav-dec" role="tabpanel" aria-labelledby="nav-dec-tab" tabindex="0">...</div>
+
+                </div>
+            </div>
+        </div>
+    </section>
     <?php include 'footer.php'; ?>
 
 </body>
