91e899cda86fcd91b18e4b51bfa7ceb56729dbbd
shalakasandcup <shalakasandcup@gmail.com>
2025-05-29 18:43:21 +0530

    captures gallery continue

--- footer.php
+++ footer.php
@@ -1,4 +1,68 @@
 <footer>
+    <div class="footer-main">
+        <div class="marquee-block right-marquee">
+            <div class="marquee-inner flex">
+                <div class="marquee-text flex">
+                    <span class="m-item flex m-title">Oluwadare Owolabi</span>
+                    <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For the People</span>
+                    <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For Progress</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For a Better Tomorrow</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">Oluwadare Owolabi</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For the People</span>
+                    <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For Progress</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For a Better Tomorrow</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                </div>
+                <div class="marquee-text flex">
+                  <span class="m-item flex m-title">Oluwadare Owolabi</span>
+                    <span class="m-item flex m-dot m-title">.</span>
+                    <span class="m-item flex m-title">For the People</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For Progress</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For a Better Tomorrow</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">Oluwadare Owolabi</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For the People</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For Progress</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                    <span class="m-item flex m-title">For a Better Tomorrow</span>
+                     <span class="m-item flex m-dot"><span></span></span>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="footer-left">
+                        <div class="subscription-box">
+                            <h5 class="text-white">Stay informed about our latest news</h5>
+                            <form action="" class="subscribe-form">
+                                <div class="form-group position-relative col-md-12">
+                                    <input type="email" class="form-control" name="email" placeholder="hello@gmail.com">
+                                   <button class="subscribe-btn"><i class="icon-Vector-Strokearrow arrw-icon one"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="images/dare-img.png" class="img-fluid" alt="dare">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <script src="./js/bootstrap.min.js"></script>
     <script src="https://unpkg.com/gsap@3/dist/gsap.min.js"></script>
     <script src="https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js"></script>
