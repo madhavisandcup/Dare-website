5f5aeb32212de4ab54b35c8f1d6f4690428394e5
Madhavi <madhavisandcup@gmail.com>
2025-06-02 16:56:52 +0530

    completed strcture of FAQS

--- index.php
+++ index.php
@@ -315,7 +315,7 @@
         </div>
     </section>
 
-    <div class="marquee-led-driven">
+    <div class="marquee-led-driven sec-space">
         <div class="marquee-block right-marquee">
             <div class="marquee-inner flex">
                 <div class="marquee-text flex">
@@ -407,7 +407,55 @@
             </div>
         </div>
     </div>
-    
+
+    <section class="faqs-wrapper sec-space overflow-hidden">
+        <div class="container">
+            <div class="center-heading text-center">
+                <h2 class="main-title w-100 text-white">Frequently asked <i class="pd_italic text_green">Questions</i></h2>
+                <p class="vision-desc m-0 text-white">Our FAQ section gives you quick solutions to frequent questions, simplifying your search for information</p>
+            </div>
+
+            <div class="accordion faqs-accordion" id="accordionExample">
+                <div class="accordion-item radius-16">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            Who is Oluwadare Owolabi?
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           Oluwadare is a public Service provider and reform-driven leader dedicated to inclusive progress, youth empowerment, and grassroots transformation.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item radius-16">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Is Oluwadare affiliated with any political party?
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           Oluwadare is a public Service provider and reform-driven leader dedicated to inclusive progress, youth empowerment, and grassroots transformation.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item radius-16">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            What are Oluwadare’s main focus areas?
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           Oluwadare is a public Service provider and reform-driven leader dedicated to inclusive progress, youth empowerment, and grassroots transformation.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section class="updates-feed sec-space overflow-hidden sm-feeds">
         <div class="container">
             <div class="row">
